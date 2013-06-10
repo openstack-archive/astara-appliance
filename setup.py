48948eb6f61b3896306db0bfc3f8b339a11488b9
Doug Hellmann <doug.hellmann@dreamhost.com>
2013-06-10 08:53:46 -0400

    fix console script name
    Change-Id: Ic23cdf8eab5079187c17ae39ad12c5c464c65ac3
    Signed-off-by: Doug Hellmann <doug.hellmann@dreamhost.com>

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setup(
             'akanda.router.commands.management:configure_ssh',
             'akanda-configure-gunicorn = '
             'akanda.router.commands.management:configure_gunicorn',
-            'akanda-akanda-configure-default-pf = '
+            'akanda-configure-default-pf = '
             'akanda.router.commands.management:configure_default_pf',
-            'akanda-api-dev-server =akanda.router.api.server:main',
+            'akanda-api-dev-server = akanda.router.api.server:main',
             'akanda-metadata-proxy = akanda.router.metadata_proxy:main',
         ]
     },
