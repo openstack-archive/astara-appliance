ee134407bfa418fb106227ea3ae89951513a7431
Ryan Petrello <ryan.petrello@dreamhost.com>
2014-02-25 14:06:16 -0500

    Add an error handler hook to embed tracebacks in the HTTP response.

--- akanda/router/api/server.py
+++ akanda/router/api/server.py
@@ -3,12 +3,14 @@
 import flask
 
 from akanda.router.api import v1
+from akanda.router.debug import handle_traceback
 from akanda.router.manager import manager
 
 app = flask.Flask(__name__)
 app.register_blueprint(v1.base.blueprint)
 app.register_blueprint(v1.system.blueprint)
 app.register_blueprint(v1.firewall.blueprint)
+app.register_error_handler(500, handle_traceback)
 
 
 @app.before_request
@@ -20,7 +22,6 @@ def attach_config():
 
 
 def main():
-    app.debug = True
     #TODO(mark): make this use a config file ie
     # app.config.from_object('akanda.router.config.Default')
     # manager.state_path = app.config['STATE_PATH']
