43fe51561523afe97265fb72f7e45129b79d6a9e
Ryan Petrello <lists@ryanpetrello.com>
2015-06-05 11:19:07 -0400

    Remove the arping dependency and send gratuitous ARP via Python's socket lib.
    Change-Id: Ib9f4f0e9165c10b5ae5ff9e26ae79c1c335489cc

--- test/unit/drivers/test_arp.py
+++ test/unit/drivers/test_arp.py
@@ -16,8 +16,10 @@
 
 
 import mock
+import socket
 import unittest2
 
+from akanda.router import models
 from akanda.router.drivers import arp
 
 config = mock.Mock()
@@ -25,6 +27,13 @@ network = mock.Mock()
 alloc = mock.Mock()
 network.address_allocations = [alloc]
 config.networks = [network]
+
+def _AF_PACKET_supported():
+    try:
+        from socket import AF_PACKET
+        return True
+    except:
+        return False
 
 
 class ARPTest(unittest2.TestCase):
@@ -83,3 +92,89 @@ class ARPTest(unittest2.TestCase):
                 mock.call('-an'),
                 mock.call('-d', '10.10.10.2')
             ])
+
+    def test_send_gratuitous_arp_for_config(self):
+        config = models.Configuration({
+            'networks': [{
+                'network_id': 'ABC456',
+                'interface': {
+                    'ifname': 'ge1',
+                    'name': 'ext',
+                },
+                'subnets': [{
+                    'cidr': '172.16.77.0/24',
+                    'gateway_ip': '172.16.77.1',
+                    'dhcp_enabled': True,
+                    'dns_nameservers': []
+                }],
+                'network_type': models.Network.TYPE_EXTERNAL,
+            }],
+            'floating_ips': [{
+                'fixed_ip': '192.168.0.2',
+                'floating_ip': '172.16.77.50'
+            },{
+                'fixed_ip': '192.168.0.3',
+                'floating_ip': '172.16.77.51'
+            },{
+                'fixed_ip': '192.168.0.4',
+                'floating_ip': '172.16.77.52'
+            },{
+                'fixed_ip': '192.168.0.5',
+                'floating_ip': '172.16.77.53'
+            }]
+        })
+
+        with mock.patch('akanda.router.utils.execute') as execute:
+            self.mgr.send_gratuitous_arp_for_floating_ips(
+                config,
+                lambda x: x.replace('ge', 'eth')
+            )
+            assert execute.call_args_list == [
+                mock.call(
+                    ['akanda-gratuitous-arp', 'eth1', '172.16.77.50'], 'sudo'
+                ),
+                mock.call(
+                    ['akanda-gratuitous-arp', 'eth1', '172.16.77.51'], 'sudo'
+                ),
+                mock.call(
+                    ['akanda-gratuitous-arp', 'eth1', '172.16.77.52'], 'sudo'
+                ),
+                mock.call(
+                    ['akanda-gratuitous-arp', 'eth1', '172.16.77.53'], 'sudo'
+                )
+            ]
+
+    @unittest2.skipIf(
+        not _AF_PACKET_supported(),
+        'socket.AF_PACKET not supported on this platform'
+    )
+    @mock.patch('socket.socket')
+    def test_send_gratuitous_arp(self, socket_constr):
+        socket_inst = socket_constr.return_value
+        socket_inst.getsockname.return_value = (
+            None, None, None, None, 'A1:B2:C3:D4:E5:F6'
+        )
+
+        self.mgr.send_gratuitous_arp('eth1', '1.2.3.4')
+        socket_constr.assert_called_once_with(
+            socket.AF_PACKET, socket.SOCK_RAW
+        )
+        socket_inst.bind.assert_called_once_with((
+            'eth1',
+            0x0806
+        ))
+        data = socket_inst.send.call_args_list[0][0][0]
+        assert data == ''.join([
+            '\xff\xff\xff\xff\xff\xff',  # Broadcast destination
+            'A1:B2:C3:D4:E5:F6',  # Source hardware address
+            '\x08\x06',  # HTYPE ARP
+            '\x00\x01',  # Ethernet
+            '\x08\x00',  # Protocol IPv4
+            '\x06',  # HADDR length, 6 for IEEE 802 MAC addresses
+            '\x04',  # PADDR length, 4 for IPv4
+            '\x00\x02',  # OPER, 2 = ARP Reply
+            'A1:B2:C3:D4:E5:F6',  # Source MAC
+            '\x01\x02\x03\x04',  # Source IP
+            'A1:B2:C3:D4:E5:F6',  # Target MAC matches
+            '\x01\x02\x03\x04'  # Target IP matches
+        ])
