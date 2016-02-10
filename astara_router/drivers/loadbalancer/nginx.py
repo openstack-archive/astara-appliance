d890f62acf70831cfed927e4bafc8dbb41c51e16
Adam Gandelman <adamg@ubuntu.com>
2016-02-09 17:37:58 -0800

    Adds keepalived based VRRPIPManager
    This adds a new IP manager driver for configuring addresses
    and routes via keepalived instead of directly.  It used when
    the logical resource is configured to be highly-available,
    according to configuration pushed by the orchestrator.
    
    We rely on a 'ha_resource' flag attached to the main config
    dict to enable it, and use specific HA config about peers and
    cluster priority contained in the 'ha_config' section of the
    main config.
    
    The resulting keepalived cluster contains a VRRP instance for
    each interface, with the exception of the management interface.
    
    Partially-implements: blueprint appliance-ha
    
    Change-Id: I5ababa41d65642b00f6b808197af9b2a59ebc67a

--- astara_router/drivers/loadbalancer/nginx.py
+++ astara_router/drivers/loadbalancer/nginx.py
@@ -12,18 +12,11 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+
 import os
 
-import jinja2
-
 from astara_router.drivers import base
-from astara_router.utils import execute
-
-
-class NginxTemplateNotFound(Exception):
-    # TODO(adam_g): These should return 50x errors and not logged
-    # exceptions.
-    pass
+from astara_router.utils import execute, load_template
 
 
 class NginxLB(base.Manager):
@@ -35,25 +28,15 @@ class NginxLB(base.Manager):
 
     def __init__(self, root_helper='sudo'):
         """
-        Initializes DHCPManager class.
+        Initializes NginxLB class.
 
         :type root_helper: str
         :param root_helper: System utility used to gain escalate privileges.
         """
         super(NginxLB, self).__init__(root_helper)
-        self._load_template()
-
-    def _load_template(self):
-        if not os.path.exists(self.CONFIG_FILE_TEMPLATE):
-            raise NginxTemplateNotFound(
-                'NGINX Config template not found @ %s' %
-                self.CONFIG_FILE_TEMPLATE
-            )
-        self.config_tmpl = jinja2.Template(
-            open(self.CONFIG_FILE_TEMPLATE).read())
+        self.config_tmpl = load_template(self.CONFIG_FILE_TEMPLATE)
 
     def _render_config_template(self, path, config):
-        self._load_template()
         with open(path, 'w') as out:
             out.write(
                 self.config_tmpl.render(loadbalancer=config)
